1af874b700ebbd39366a8bd995f4dd5a0bf5e3de
marinebel <marinebeltus@gmail.com>
2021-02-24 14:10:41 +0100

    init the template tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+purge: ["**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      "display" : ["Roboto"]
+    },
+    extend: {
+      colors: {
+        "primary" : "#c0392b",
+        "secondary" : "#2c3e50"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    
+  ],
+}
